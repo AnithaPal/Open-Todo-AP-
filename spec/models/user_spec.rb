502784c95125e5cedd1fa5b1cc7ccb11fd08bcf9
AnithaPal <anitha.aravindvel@gmail.com>
2015-07-18 22:08:54 -0400

    user spec added

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,46 @@
 require 'rails_helper'
 
 RSpec.describe User, :type => :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "is valid with a username, email and a password" do
+    user = User.new(
+      username: 'Ava',
+      email: 'ava@example.com',
+      password: 'helloworld')
+    expect(user).to be_valid
+  end  
+
+  it "is invalid without a username" do
+    user = User.new(
+      username: nil)
+    user.valid?
+    expect(user.errors[:username]).not_to include("can't be blank")
+  end  
+
+  it "is invalid without an email" do
+    user = User.new(
+      email: nil)
+    user.valid?
+    expect(user.errors[:email]).not_to include("can't be blank")
+  end  
+
+  it "is invalid without a password" do
+    user = User.new(
+      password: nil)
+    user.valid?
+    expect(user.errors[:password]).not_to include("can't be blank")
+  end  
+
+  it "is invalid with a duplicate email address" do
+    User.create(
+      username: 'Anne',
+      email: 'tester@example.com',
+      password: 'helloworld')
+
+    user = User.new(
+      username: 'Ava',
+      email: 'tester@example.com',
+      password: 'helloworld')
+    user.valid?
+    expect(user.errors[:email]).to include("has already been taken")
+  end    
 end
