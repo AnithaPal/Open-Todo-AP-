bf5d9fa9c7027aac1f301018e57228af316f7a8a
AnithaPal <anitha.aravindvel@gmail.com>
2015-07-18 22:19:49 -0400

    item spec added

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,6 @@
 class Item < ActiveRecord::Base
   belongs_to :list
   has_many :items
+  
+  validates :name, presence: true
 end
--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -1,5 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe Item, :type => :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "is valid with a item name" do
+    item = Item.new(
+      name: "clean the kitchen cabinet")
+    expect(item).to be_valid
+  end  
+  it "is invalid without an item name" do
+    item = Item.new(name: nil)
+    expect(item.errors[:name]).not_to include("can't be blank")
+  end  
+
+
 end
