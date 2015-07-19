a2850739af0e5d81177e55cb8db05c961c4fab2a
AnithaPal <anitha.aravindvel@gmail.com>
2015-07-18 20:56:59 -0400

    user, list and item models created

--- config/application.rb
+++ config/application.rb
@@ -34,14 +34,14 @@ module OpenTodoApi
 
     #rspec configuration
     config.generators do |g|
-        g.test_framework :rspec
+        g.test_framework :rspec,
         fixtures: true,
         view_specs: false,
         helper_specs: false,
         routing_specs: false,
         controller_specs: true,
-        request_specs: false,
-        g.fixture_replacement :factory_girl: "spec/factories"
-    end    
-  end
-end
+        request_specs: false
+        g.fixture_replacement :factory_girl, dir: "spec/factories"
+     end
+    end 
+ end   
