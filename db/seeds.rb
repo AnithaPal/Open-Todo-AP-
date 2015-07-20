d7f4bcd5de23407669fcf0181cf043b4dfb4bef6
AnithaPal <anitha.aravindvel@gmail.com>
2015-07-20 11:46:52 -0400

    serialized user, list and item models

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,47 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+require 'faker'
+
+3.times do 
+  user = User.new(
+    username: Faker::Name.name,
+    email: Faker::Internet.free_email,
+    password: Faker::Lorem.characters(8))
+  user.save!
+
+end
+
+user = User.new(
+    username: 'Admin',
+    email: 'admin@example.com',
+    password: 'helloworld')
+user.save!
+
+
+users = User.all
+
+5.times do 
+
+list = List.create!(
+  user: users.sample,
+  name: Faker::Lorem.sentence)
+end
+
+
+lists = List.all
+
+20.times do
+
+item = Item.create!(
+  list: lists.sample,
+  name: Faker::Lorem.sentence,
+  completed: false)
+end
+
+puts "Seed finsihed"
+puts "#{User.count} users created"
+puts "#{List.count} lists created"
+puts "#{Item.count} items created"
+
